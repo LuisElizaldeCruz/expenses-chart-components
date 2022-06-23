1095124ad45a7d9894f4b3761d57b9bb613740db
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-06-23 17:43:40 -0500

    se corrigo la obtencion de datos para la creacion de la grafica

--- js/script.js
+++ js/script.js
@@ -21,10 +21,8 @@
 })();
 */
 
-
-
 const usuarios = document.querySelector(".usuarios");
-fragmento = document.createDocumentFragment();
+//fragmento = document.createDocumentFragment();
 let dias = [];
 let monto = [];
 
@@ -34,8 +32,53 @@ fetch("http://127.0.0.1:5500/data.json")
         return res.ok ? res.json() : Promise.reject(res);//ya la va a devolver en formato json
     })
     .then(json => {
-        //console.log(json);
-        recorrerObjeto(json);
+        json.forEach((el) => {
+            const li = document.createElement("li");
+          //  li.innerHTML = `${el.day} -- ${el.amount}`;
+          //  fragmento.appendChild(li);
+
+            dias.push(el.day);
+            monto.push(el.amount);
+        });
+        //usuarios.appendChild(fragmento);
+        /*empiza el dibujado de la grafica*/
+        let arrD = dias;
+        let arrM = monto;
+
+        console.log(arrD);
+        console.log(arrM);
+
+        let micanvas = document.getElementById("miGrafica").getContext("2d");
+        let chart = new Chart(micanvas, {
+            type: "bar",
+            data: {
+                labels: arrD,
+                //labels: ["lunes", "martes", "miercols","jueves"],
+                datasets: [
+                    {
+                        //label: "grafica de datos",
+                        data: arrM,
+                        //data: [5, 10, 20, 30],
+                        backgroundColor: "hsl(10, 79%, 65%)"
+                    }
+                ]
+            },
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Gasto de los ultimos 7 dias'
+                    },
+                    legend: {
+                        display: false,
+                        labels: {
+                            color: 'rgb(255, 99, 132)'
+                        }
+                    }
+                }
+            }
+        });
+        /*termina el dibujado de la grafica*/
     })
     .catch((err) => {
         console.log("estamos en el catch ", err);
@@ -45,58 +88,3 @@ fetch("http://127.0.0.1:5500/data.json")
         // console.log("Esto se ejecutara independientemente del resultado de la promesa fetch");
     });
 
-const recorrerObjeto = (objs) => {
-
-    // console.log("este es el objeto sacado", objs);
-    objs.forEach((el) => {
-        const li = document.createElement("li");
-        li.innerHTML = `${el.day} -- ${el.amount}`;
-        fragmento.appendChild(li);
-
-        dias.push(el.day);
-        monto.push(el.amount);
-    });
-    usuarios.appendChild(fragmento);
-
-}
-
-function grafica() {
-    let arrD = dias;
-    let arrM = monto;
-    console.log(arrM);
-
-    let micanvas = document.getElementById("miGrafica").getContext("2d");
-    let chart = new Chart(micanvas, {
-        type: "bar",
-        data: {
-            labels: arrD,
-            //labels: ["lunes", "martes", "miercols","jueves"],
-            datasets: [
-                {
-                    label: "grafica de datos",
-                    data: arrM,
-                    //data: [5, 10, 20, 30],
-                    backgroundColor: "hsl(10, 79%, 65%)"
-                }
-            ]
-        },
-        options: {
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Gasto de los ultimos 7 dias'
-                },
-                legend: {
-                    display: false,
-                    labels: {
-                        color: 'rgb(255, 99, 132)'
-                    }
-                }
-
-            }
-        }
-
-    });
-}
-
-grafica();
